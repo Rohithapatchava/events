51a4acdee42edcbd87101f506a9763b294e547b5
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:42:43 +0530

    Initial commit

---
+++ events/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace events
+{
+    class Program
+    {
+        public delegate void Deleventhandler();
+        public static event Deleventhandler handler;
+        static void Main(string[] args)
+        {
+            handler += new Deleventhandler(Square);
+            handler += new Deleventhandler(Circle);
+            handler.Invoke();
+        }
+        
+        static void Circle()
+            {
+                double r = 12.5;
+                double ar = Math.PI * r * r;
+                Console.WriteLine("area of circle:{0}", ar);
+            }
+
+        static void Square()
+        {
+            double r = 5.9;
+            double ar = r * r;
+            Console.WriteLine("area of square: {0}", ar);
+
+        }
+    }
+}
